45c0a83771aa517b6338fe663ad92265e19db51d
J-Inacio <j.inacio.s@hotmail.com>
2025-11-22 01:03:14 -0300

    fix tailwind version incompatibility

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 const DefaultLayout = () => {
   return (
     <>
-      <h1>teste default layout</h1>
+      <h1 className="">teste default layout</h1>
       <Outlet />
     </>
   );
